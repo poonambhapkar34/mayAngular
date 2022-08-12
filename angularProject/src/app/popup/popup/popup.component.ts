b74f762ce04af9c42d579bbae1488b4b029b8ef9
poonam <poonambhapkar34@gmail.com>
2022-08-12 22:19:35 +0530

    observable and promise

--- angularProject/src/app/popup/popup/popup.component.ts
+++ angularProject/src/app/popup/popup/popup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { of } from 'rxjs';
+import { Observable, of } from 'rxjs';
 import { DataService } from 'src/app/data.service';
 
 @Component({
@@ -11,15 +11,46 @@ export class PopupComponent implements OnInit {
 
   apiData:any;
   cars$ : any;
+  carFlag =false;
+
+  
   carNames = ['minicup','volvo','jaguar','i10','i20'];
   datalist: any;
+  cData: any;
+  obs2: any;
+  obs1: any;
+;
 
 
   constructor(private dataService : DataService) { }
 
 
   ngOnInit(): void {
-    this.cars$ = of(this.carNames);
+
+ this.obs2 =   new Observable ((obser)=>{
+      setTimeout(() => {
+        obser.next('in process')
+      }, 3000);
+    }).subscribe(data=>{
+      console.log(data);
+      
+    })
+    
+  }
+
+  getObservableData(){
+    console.log('cars var before making it obs',this.cars$);
+   return this.obs1 = this.cars$ = of(this.carNames);
+    console.log('cars var after making it obs',this.cars$);
+  }
+  getObsrData(){
+    this.getObservableData().subscribe((carData)=>{
+      console.log(carData);
+      this.carFlag = true;
+     this.cData =carData;
+      
+    });
+
 
   }
 
@@ -50,9 +81,27 @@ export class PopupComponent implements OnInit {
   }
   ngOnDestroy(){
     console.log('destor calling to unsubscribe');
-    
     this.datalist.unsubscribe();
+   // this.obs1.unsubscribe();
+    this.obs2.unsubscribe();
 
   }
+  getPromises(){
+
+    let pen = new Promise ((resolve, reject)=>{
+   if(!true){
+    resolve('resolved...got product')
+   }
+   else reject('not got')          
+    })
+    pen.then((msg)=>{
+      console.log(msg);    
+    }).catch(res=>{
+      console.log(res);
+      
+    })
+  }
+
+
 
 }
